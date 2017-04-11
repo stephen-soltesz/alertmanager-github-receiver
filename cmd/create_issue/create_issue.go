fe7d8e794e46f4248d8cf04e07b7619b57b23ad4
Stephen Soltesz <stephen.soltesz@gmail.com>
2017-04-11 00:15:00 -0400

    clean up

--- cmd/create_issue/create_issue.go
+++ cmd/create_issue/create_issue.go
@@ -13,108 +13,87 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 )
 
 var (
 	authtoken          = flag.String("authtoken", "", "Oauth2 token for access to github API.")
-	create             = flag.Bool("create", false, "Create a new issue.")
-	list               = flag.Bool("list", false, "List open issues.")
 	githubOwner        = flag.String("github-owner", "stephen-soltesz", "Probably the same as github organization.")
 	githubRepo         = flag.String("github-repo", "public-issue-test", "The repository name for issues.")
 	githubRecentIssues = flag.Int("github-recent-issues", 24, "Search issues created in the last N hours.")
 	client             *github.Client
 )
 
-/*
- * Receive alert.
- * List alerts from github for the last hour from issue repo.
- * if alert is firing, and issue does not exist, then create new issue.
- * if alert is resolved, and issue exists and open, close it.
- */
+// alertReceiverHandler handles AM notifications.
 func alertReceiverHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: verify that method is POST.
+	// Verify that request is a POST.
+	if r.Method != http.MethodPost {
+		log.Printf("Client used unsupported method: %s: %s", r.Method, r.RemoteAddr)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+	// Read request body.
 	alertBytes, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		panic(err)
+		log.Printf("Failed to read request body: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
-	defer r.Body.Close()
 
 	// Parse webhook message.
 	msg := &notify.WebhookMessage{}
 	if err := json.Unmarshal(alertBytes, msg); err != nil {
-		log.Printf("Failed to parse webhook message from <IP>: %s", err)
+		log.Printf("Failed to parse webhook message from %s: %s", r.RemoteAddr, err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	fmt.Printf("%#v\n", msg)
+	// Print a debug message.
 	pretty.Print(msg)
 
-	log.Printf("Handling alert: 0x%x", msg.GroupKey)
-	err = handleAlert(msg)
-	if err != nil {
-		log.Printf("Failed to handle alert: 0x%x: %s", msg.GroupKey, err)
+	// Handle the webhook message.
+	log.Printf("Handling alert: %s", id(msg))
+	if err := handleAlert(msg); err != nil {
+		log.Printf("Failed to handle alert: %s: %s", id(msg), err)
 	}
-}
-
-// formatTitle constructs an issue title from a webhook message.
-func formatTitle(msg *notify.WebhookMessage) string {
-	return fmt.Sprintf("[0x%x] %s\n", msg.GroupKey, msg.Data.GroupLabels["alertname"])
-}
-
-// formatIssueBody constructs an issue body from a webhook message.
-func formatIssueBody(msg *notify.WebhookMessage) string {
-	return fmt.Sprintf("Original alert: %s\nTODO: add graph url from annotations.", msg.ExternalURL)
+	log.Printf("Completed alert: %s", id(msg))
 }
 
 // handleAlert performs all handling of a webhook message.
 func handleAlert(msg *notify.WebhookMessage) error {
-	// * List alerts from github for the last hour from issue repo.
-	title := formatTitle(msg)
-	log.Printf("Handling message: %s", title)
+	// List known issues from github.
 	issues, err := listIssues()
 	if err != nil {
 		return err
 	}
+
+	// Search for an issue that matches the notification message from AM.
+	msgTitle := formatTitle(msg)
 	var foundIssue *github.Issue
 	for _, issue := range issues {
-		if title == *issue.Title {
-			log.Printf("Found title: %s\n", title)
+		if msgTitle == *issue.Title {
+			log.Printf("Found matching issue: %s\n", msgTitle)
 			foundIssue = issue
 			break
 		}
 	}
-	if msg.Data.Status == "firing" {
-		log.Printf("Firing alert: 0x%x", msg.GroupKey)
-		// if alert is firing and missing from github, create new issue.
-		if foundIssue == nil {
-			issue, err := createIssue(title, msg)
-			if err != nil {
-				return err
-			}
-			log.Printf("Created new issue: %#v\n", issue)
-		}
-	} else if msg.Data.Status == "resolved" {
-		log.Printf("Resolving alert: 0x%x", msg.GroupKey)
-		// if alert is resolved and present in github, close issue.
-		// - NOTE: there will be multiple resolved messages because prometheus
-		// continues to evaluate rules every `evaluation_interval`. And,
-		// alertmanager preserves an alert until `resolve_timeout`.
-		// resolve_timeout (alertmanager.yml) / evaluation_interval (prometheus.yml)
-		// But, as long as we only act on "open" issues, it should be a no-op.
-		if foundIssue != nil && *foundIssue.State == "open" {
-			log.Printf("Closing issue: %#v\n", foundIssue)
-			err = closeIssue(foundIssue)
-			if err != nil {
-				return err
-			}
-		}
-	} else {
-		log.Printf("Unsupported Status: %s", msg.Data.Status)
+
+	// The message is currently firing and we did not find a matching
+	// issue from github, so create a new issue.
+	if msg.Data.Status == "firing" && foundIssue == nil {
+		_, err := createIssue(msgTitle, msg)
+		return err
 	}
-	log.Printf("Finished handling alert: 0x%x", msg.GroupKey)
-	return nil
+
+	// The message is resolved and we found a matching open issue from github,
+	// so close the issue.
+	if msg.Data.Status == "resolved" && foundIssue != nil {
+		// NOTE: there will be multiple "resolved" messages for the same
+		// alert. Prometheus evaluates rules every `evaluation_interval`.
+		// And, alertmanager preserves an alert until `resolve_timeout`. So
+		// expect (resolve_timeout / evaluation_interval) messages.
+		return closeIssue(foundIssue)
+	}
+
+	return fmt.Errorf("Unsupported WebhookMessage.Data.Status: %s", msg.Data.Status)
 }
 
 // issueViewerHandler lists all issues from github.
@@ -122,13 +101,27 @@ func issueViewerHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html><body>okay</body></html>")
 }
 
+func id(msg *notify.WebhookMessage) string {
+	return fmt.Sprintf("0x%x", msg.GroupKey)
+}
+
+// formatTitle constructs an issue title from a webhook message.
+func formatTitle(msg *notify.WebhookMessage) string {
+	return fmt.Sprintf("[%s] %s\n", id(msg), msg.Data.GroupLabels["alertname"])
+}
+
+// formatIssueBody constructs an issue body from a webhook message.
+func formatIssueBody(msg *notify.WebhookMessage) string {
+	return fmt.Sprintf("Original alert: %s\nTODO: add graph url from annotations.", msg.ExternalURL)
+}
+
 // listIssues from github.
 func listIssues() ([]*github.Issue, error) {
 	var allIssues []*github.Issue
 	ctx := context.Background()
 
-	log.Printf("Listing issues")
-	opts := &github.IssueListByRepoOptions{Since: time.Now().Add(-(time.Duration(*githubRecentIssues)) * time.Hour)}
+	log.Printf("Listing issues:")
+	opts := &github.IssueListByRepoOptions{State: "open"} // Since: time.Now().Add(-(time.Duration(*githubRecentIssues)) * time.Hour)}
 	for {
 		issues, resp, err := client.Issues.ListByRepo(ctx, *githubOwner, *githubRepo, opts)
 		if err != nil {
@@ -136,7 +129,7 @@ func listIssues() ([]*github.Issue, error) {
 			return nil, err
 		}
 		for _, issue := range issues {
-			fmt.Printf("%s / %d\n", *issue.Title, *issue.Number)
+			fmt.Printf("  %s / %d\n", *issue.Title, *issue.Number)
 			allIssues = append(allIssues, issue)
 		}
 		if resp.NextPage == 0 {
@@ -144,46 +137,40 @@ func listIssues() ([]*github.Issue, error) {
 		}
 		opts.ListOptions.Page = resp.NextPage
 	}
-	log.Printf("Returning all issues")
 	return allIssues, nil
 }
 
 // createIssue on github.
 func createIssue(title string, msg *notify.WebhookMessage) (*github.Issue, error) {
 	log.Printf("Creating issue: %s", title)
-	ctx := context.Background()
 	body := formatIssueBody(msg)
 	issueReq := github.IssueRequest{
 		Title: &title,
 		Body:  &body,
 	}
 
-	issue, _, err := client.Issues.Create(ctx, *githubOwner, *githubRepo, &issueReq)
+	// Create the issue.
+	issue, _, err := client.Issues.Create(context.Background(), *githubOwner, *githubRepo, &issueReq)
 	if err != nil {
-		log.Printf("Error creating issue: %s", err)
 		return nil, err
 	}
 
-	// fmt.Println(github.Stringify(resp))
-	fmt.Println(github.Stringify(issue))
+	log.Printf("Created new issue: %s\n", pretty.Sprint(issue))
 	return issue, nil
 }
 
 // closeIssue on github.
 func closeIssue(issue *github.Issue) error {
-	ctx := context.Background()
+	log.Printf("Closing issue: %s\n", *issue.Title)
 	state := "closed"
 	issueReq := github.IssueRequest{
 		State: &state,
 	}
-	// TODO: add comment about what is happening.
 
-	// _, _, err := client.Issues.Edit(ctx, *githubOwner, *githubRepo, *issue.Number, &issueReq)
-	_, _, err := client.Issues.Edit(ctx, *githubOwner, *githubRepo, *issue.Number, &issueReq)
-	if err != nil {
-		return err
-	}
-	return nil
+	// Set the issue state to "closed".
+	_, _, err := client.Issues.Edit(context.Background(), *githubOwner, *githubRepo, *issue.Number, &issueReq)
+	log.Printf("Closed issue: %s\n", *issue.Title)
+	return err
 }
 
 func serveListener() {
